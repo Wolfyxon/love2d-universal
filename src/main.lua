09381dce73e5bfb54e1adb349e4d19d9c8e96f8a
Wolfyxon <wolfyxon@gmail.com>
2024-07-17 14:42:01 +0200

    Test image is rendered

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,11 @@
+local testImg = love.graphics.newImage( "test.png")
+local testImgW, testImgH = testImg:getDimensions()
+
 function love.draw(screen)
+    local width, height = love.graphics.getDimensions(screen)
+
     love.graphics.print('Hello World!', 0, 0)
+    love.graphics.draw(testImg, width / 2 - testImgW / 2, height / 2 - testImgH / 2)
 end
 
 function love.gamepadpressed(joystick, button)
