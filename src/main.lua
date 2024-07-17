7dded77b50ed72ae51a20e425ff242bd7aa4e5d6
Wolfyxon <wolfyxon@gmail.com>
2024-07-17 14:43:39 +0200

    Hello World and test image are rendered on separate screens on 3DS

--- src/main.lua
+++ src/main.lua
@@ -4,8 +4,13 @@ local testImgW, testImgH = testImg:getDimensions()
 function love.draw(screen)
     local width, height = love.graphics.getDimensions(screen)
 
-    love.graphics.print('Hello World!', 0, 0)
-    love.graphics.draw(testImg, width / 2 - testImgW / 2, height / 2 - testImgH / 2)
+    if not screen or screen == "top" then
+        love.graphics.print('Hello World!', 0, 0)
+    end
+    
+    if not screen or screen == "bottom" then
+        love.graphics.draw(testImg, width / 2 - testImgW / 2, height / 2 - testImgH / 2)
+    end
 end
 
 function love.gamepadpressed(joystick, button)
